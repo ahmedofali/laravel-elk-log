2ef551dbd31d8d9d402846344780187390f42812
ahmed ali <dev.ahmed.abbas@gmail.com>
2019-11-19 13:27:05 +0200

    change package name

--- src/ElkServiceProvider.php
+++ src/ElkServiceProvider.php
@@ -4,6 +4,7 @@ namespace Ahmedofali\LaravelElkLog;
 
 use Elasticsearch\Client;
 use Elasticsearch\ClientBuilder;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Monolog\Formatter\ElasticsearchFormatter;
 
@@ -22,15 +23,69 @@ class ElkServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        dd( 'aj' );
+        $this->mergeConfigFrom( __DIR__.'/../config/log.php', 'logging' );
+
         $this->app->singleton(Client::class, function ($app) {
-            $url = "http://asalahnew_elasticsearch_1:9200";
+            $url = $this->getHost();
 
             return ClientBuilder::create()->setHosts([$url])->build();
         });
 
         $this->app->bind(ElasticsearchFormatter::class, function ($app) {
-            return new ElasticsearchFormatter('ahmed', '_doc');
+            return new ElasticsearchFormatter( $this->getIndexName(), $this->getIndexType() );
         });
 
+    }
+
+    /**
+     * get index type
+     *
+     * @return string
+     */
+    public function getIndexType(): string
+    {
+        if( App::environment(['local', 'staging']) )
+        {
+            return env('ELK_TYPE_LOCAL', '_doc');
+        }
+        else
+        {
+            return env('ELK_TYPE_LOCAL', '_doc');
+        }
+    }
+
+    /**
+     * get index name
+     *
+     * @return string
+     */
+    public function getIndexName(): string
+    {
+        if( App::environment(['local', 'staging']) )
+        {
+            return env('ELK_INDEX_LOCAL', 'elastic_local');
+        }
+        else
+        {
+            return env('ELK_INDEX_LIVE', 'elastic_local');
+        }
+    }
+
+    /**
+     * get index host
+     *
+     * @return string
+     */
+    public function getHost(): string
+    {
+        if( App::environment(['local', 'staging']) )
+        {
+            return env('ELK_HOST_LOCAL', 'http://localhost:9200');
+        }
+        else
+        {
+            return env('ELK_HOST_LIVE', 'http://localhost:9200');
+        }
     }
 }
